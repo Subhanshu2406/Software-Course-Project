84ff9ccf4483433e551b2009938b116a9754b8be
Subhanshu2406 <178010453+Subhanshu2406@users.noreply.github.com>
2026-04-03 13:48:52 +0530

    Update replication: follower registration and replication handler

--- shard/replication/follower.go
+++ shard/replication/follower.go
@@ -59,7 +59,6 @@ func (f *FollowerReceiver) LastLogID() uint64 {
 // RegisterHTTPHandlers adds replication endpoints to a ServeMux.
 func (f *FollowerReceiver) RegisterHTTPHandlers(mux *http.ServeMux) {
 	mux.HandleFunc("/replicate", f.handleReplicate)
-	mux.HandleFunc("/log-index", f.handleLogIndex)
 }
 
 // --- HTTP handlers ---
